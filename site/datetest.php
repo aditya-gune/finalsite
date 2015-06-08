db899bb78d230cc163bab27afb7e429dcd0313d9
agune001 <ca_adi@yahoo.com>
2015-06-08 01:28:31 -0400

    deleted unnecessary files
    deleted unused files

--- site/datetest.php
+++
@@ -1,6 +0,0 @@
-Date Applied<br /> <input id="date_applied" type="text" name="date_applied" placeholder="mm/dd/yyyy" onchange="return checkDate(this)"><br>
-<?php
- $rawdate = isset($_POST['date_applied'])? $_POST['date_applied']: '';
- $xplod = explode('/', $rawdate);
-print_r($xplod);
-?>
